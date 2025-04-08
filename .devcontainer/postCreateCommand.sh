8f20e31c7be27a0aa8260321c83c5a807a6802ac
Juan Cerezo <7447739+juancerezo@users.noreply.github.com>
2025-04-08 15:44:44 +0200

    Config IDE

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo -u vscode bash << EOF
+pip install --no-warn-script-location --user -e .[dev]
+git config --unset core.hookspath
+pre-commit install
+rm -Rf *.egg-info build data
+python -m backend database create
+alembic upgrade head
